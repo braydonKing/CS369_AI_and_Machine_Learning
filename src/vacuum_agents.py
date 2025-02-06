9755b9812a8876cbafabec631e7afb6841953fab
braydonKing <4bking.2@gmail.com>
2025-02-06 14:49:31 -0800

    added way for bot to switch directions in a sweeping pattern. Very simple method using a counter.

--- src/vacuum_agents.py
+++ src/vacuum_agents.py
@@ -3,8 +3,10 @@ import random
 
 
 directions = ["east", "west", "north", "south"]
-last_square_status = None
+last_square_status = True
 last_movement = None
+switch = 1
+count = 0
 
 def reflex_agent(percept):
     if percept:
@@ -21,9 +23,36 @@ def random_agent(percept):
     return action
 
 def state_agent(percept):
-#allowed to remember:
+    #allowed to remember:
     # what it has seen (dirty or clean square)
     # what it has done (which direction it has moved)
+    global last_movement,last_square_status,switch,count
 
-run(20,5000,state_agent)
-print(many_runs(20, 50000, 1, random_agent))
+    if percept:
+        action = 'clean'
+        last_square_status = percept
+    elif not percept and switch == 1:
+        action = 'north'
+    elif not percept and switch == -1:
+        action = 'south'
+
+    if last_square_status == False and percept == False:
+        if count>100:
+            action = 'west'
+            switch = switch * -1
+        else:
+            action = 'east'
+            switch = switch * -1
+        if count>150:
+            count = 0
+        count += 1
+        #print(count)
+
+    last_square_status = percept
+    #print(last_square_status)
+    #print(count)
+    return action
+
+
+#run(20,5000,state_agent)
+print(many_runs(20, 50000, 3, state_agent))
