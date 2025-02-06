819dd872adb016743239d57ae0e71f5f1ce5c2bb
braydonKing <4bking.2@gmail.com>
2025-02-05 17:21:48 -0800

    first commit :D

--- src/vacuum_agents.py
+++ src/vacuum_agents.py
@@ -1,26 +1,43 @@
 from vacuum import *
 import random
 
-def reflex_agent(percept):
-    if percept == True:
-        action = 'clean'
-    if percept == False:
-        action = 'east'
 
+directions = ["east", "west", "north", "south"]
+last_square_status = None
+last_movement = None
+
+def reflex_agent(percept):
+    if percept:
+        action = 'clean'
+    else:
+        action = 'east'
     return action
 
 def random_agent(percept):
-    directions = ["east", "west", "north", "south"]
-
-    if percept == True:
+    if percept:
         action = 'clean'
-    if percept == False:
+    else:
         action =  random.choice(directions)
     return action
 
-def state_agent():
-    
-    return 0
+def state_agent(percept):
+# allowed to remember:
+    # what it has seen (dirty or clean square)
+    # what it has done (which direction it has moved)
 
-percept=None
-run(20, 5000,random_agent)
+    if percept:
+        action = 'clean'
+    elif not percept and switch == 1: #if clean and switch is 0
+        action = 'north'
+    elif not percept and switch == -1: #if clean and switch is 0
+        action = 'south'
+
+    if last_square_status == False & percept == False:
+        action = 'east'
+
+    print(last_square_status)
+    print(last_movement)
+    return action
+
+run(20,5000,state_agent)
+print(many_runs(20, 50000, 1, random_agent))
