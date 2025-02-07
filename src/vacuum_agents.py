9f8c88a138cf276474e7fc0e35650094922b79eb
braydonKing <4bking.2@gmail.com>
2025-02-06 21:09:33 -0800

    added comments and simplified code.

--- src/vacuum_agents.py
+++ src/vacuum_agents.py
@@ -1,5 +1,13 @@
+from html.parser import commentclose
+
 from vacuum import *
 import random
+
+"""
+Author: <Braydon King>
+
+Simple vacuum AI's. No specific algorithm used
+"""
 
 
 directions = ["east", "west", "north", "south"]
@@ -28,9 +36,11 @@ def state_agent(percept):
     #allowed to remember:
     # what it has seen (dirty or clean square)
     # what it has done (which direction it has moved)
+    # counters okay??
+
     global last_movement,last_square_status,switch,count,side_direction
 
-    if percept:
+    if percept: #if dirty, clean square. Otherwise, move 'north' or 'south'.
         action = 'clean'
         last_square_status = percept
     elif not percept and switch == 1:
@@ -38,28 +48,22 @@ def state_agent(percept):
     elif not percept and switch == -1:
         action = 'south'
 
-    if last_square_status == False and percept == False:
-        if count<250:
+    if last_square_status == False and percept == False: #if last square status was clean and the current square status is clean, the bot is essentially stuck on a wall.
+        if count<15:  #assume bot gets in "lucky" position with less walls and chooses a predictable direction 'west'.
             action = 'west'
-
-        else:
-            action = 'east'
-
-        if count>15:
+        else: #FAILSAFE creates random action if bot is stuck/cant find dirt.
             action = random.choice(directions)
 
-        if count>500:
+        if count>500: #reset counter so it can keep trying horizontal direction 'west' on occasion.
             count = 0
 
-        switch = switch * -1
-        count += 1
-        #print(count)
+        switch = switch * -1 #switch for vertical directions
+        count += 1 #count to track random or predictable movement
 
-    last_square_status = percept
-    #print(last_square_status)
-    #print(count)
+    last_square_status = percept #set last square status
+
     return action
 
 
 #run(20,5000,state_agent)
-print(many_runs(20, steps, 10, state_agent))
+print(many_runs(20, 50000, 10, state_agent))
